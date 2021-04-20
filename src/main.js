3b0cbf96bea78e1d1f54a2f46ae1c4b0f840ca52
cgbin <1109092897@qq.com>
2021-04-20 17:22:05 +0800

    use ssh

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+//全局引入elemente
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/css/global.css'
+
 import 'default-passive-events'
 
 Vue.config.productionTip = false
